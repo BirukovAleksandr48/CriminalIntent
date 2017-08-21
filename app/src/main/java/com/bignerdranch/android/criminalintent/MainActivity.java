159691daed1ee32c1c018eda9f49d772843afa89
BirukovAleksandr48 <birukov.aleksandr48@gmail.com>
2017-08-21 12:12:56 +0300

    proshel 8 glav

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
@@ -1,13 +1,24 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
-//12
+public class MainActivity extends FragmentActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 }
